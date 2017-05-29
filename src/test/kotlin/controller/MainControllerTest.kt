2a3e0d1552b44fd8d5ce85271feb1ccd8b459f8a
Michael Ohayon <mohayon75@gmail.com>
2017-05-29 15:07:24 +0200

    gradlew execution right fix. Added empty test Circle.yml update

---
+++ src/test/kotlin/controller/MainControllerTest.kt
@@ -0,0 +1,14 @@
+package controller
+
+import org.junit.Test
+
+/**
+ * Created by mohayon on 29/05/2017.
+ */
+class MainControllerTest {
+
+    @Test
+    fun emptyTest() {
+        assert(2 + 3 == 3 + 2)
+    }
+}
